e17b858046cf47132a4c6ffa0546032171e1c152
youyoumubei <youyoumubei@126.com>
2023-04-27 15:02:59 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 from flask import Flask
-from api import api_routes  # 导入包含API路由的文件
+from api import bp as api_bp
+from api2 import bp as api2_bp
 
 app = Flask(__name__)
-app.register_blueprint(api_routes)  # 添加API路由到应用程序对象
+
+app.register_blueprint(api_bp, url_prefix='/api/v1')
+app.register_blueprint(api2_bp, url_prefix='/api/v2')
 
 if __name__ == '__main__':
     app.run(port=8999)
